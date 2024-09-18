efb0cdaf5f002d2aafd41106b27b42d28c12d00b
Vinay Kumar Kosgi <vinaykumar.kosgi@outlook.com>
2024-09-18 15:12:26 +0530

    changed host

--- app.py
+++ app.py
@@ -279,4 +279,5 @@ def stock_detail(symbol):
         return jsonify({"error": "Unable to fetch stock data"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True, use_reloader=False)
